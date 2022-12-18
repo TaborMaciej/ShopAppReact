45f187a9500290b52bf38680fb7e58d4340d3766
Daisy <gos434@onet.eu>
2022-12-18 11:38:33 +0100

    header
    Still working on Nav look, made some changes in .js and .css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -14,13 +14,13 @@ function Header(){
                     <div className="search-bar">
                         <img className="nav-img" src={GlassIMG} alt="Search"/>
                         <input type="text"/>
-                   
-                    <Link to="/Cart"><img className="nav-img" src={CartIMG} alt="Cart"/></Link>
-                 
-                    <img className="nav-img" src={UserIMG} alt="User"/>
-                    
-                    <Link to="/Login">Sign in </Link>
-                    <Link to="/register">Register</Link>
+                     
+                        <Link to="/Cart"><img className="nav-img" src={CartIMG} alt="Cart"/></Link>
+                        <img className="nav-img" src={UserIMG} alt="User"/>                   
+                        <p><Link to="/Login">Sign in </Link></p>
+                        <p><Link to="/register">Register</Link></p>
+                  
+
                     </div>
             </nav>
 
