eeced97dda966aabadfbeccdd45e2d24c1ccb8f7
Maciej Tabor <b.maciejtabor@gmail.com>
2023-01-05 16:16:14 +0100

    Added image display for the frontend
    Also fixed login bug caused by changes in database structure

--- client/src/components/SingularProduct.js
+++ client/src/components/SingularProduct.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "../css/Product.css";
 
+
 function SingularProduct(props) {
   return (
       <div className="product-box">
-        <img src={"../imgs/okladki_gier/" + props.data.path} alt={"Zdjecie gry: "+props.data.Nazwa_gry}/>
+        <img src={ require("../imgs/okladki_gier/" + props.data.Path) } alt={"Zdjecie gry: " + props.data.Nazwa_gry} className="product-img"/>
         <h3>{props.data.Nazwa_gry}</h3>
         <p>Gatunek: {props.data.Gatunek}</p>
         <p>Wydawnictwo: {props.data.Wydawnictwo}</p>
