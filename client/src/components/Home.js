82c3cc5569884be661f8917cf0d461091eb6e6f1
Daisy <gos434@onet.eu>
2023-01-06 15:53:27 +0100

    made advertisment
    meme haha

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,14 +3,32 @@ import { DataContext } from "../App.js"
 import Axios from 'axios';
 import Product from "./SingularProduct.js"
 import "../css/Home.css"
+import meme from "../imgs/meme.png";
+import wyprzedaz from "../imgs/wyprzedaz.png";
 
 function Home() {
+
 
 
   const { gameData, SetGameData } = useContext(DataContext);
   const [selectRequest, SetSelectRequest] = useState(true);
   let keys = {}
-  
+  const slides = [
+    <img id="slide1" alt="reklama" src={wyprzedaz} />,
+    <img id="slide2" alt="meme" src={meme} />,
+  ];
+
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSlideIndex((prevIndex) => (prevIndex + 1) % 2);
+    }, 3000); // 3 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+
   useEffect(() => {
     
     SetGameData([]);
@@ -35,12 +53,16 @@ function Home() {
           console.log(err);
         }
       });
-  
+
+
+
     return () => {
       source.cancel();
     };
     // eslint-disable-next-line
   }, []);
+
+
   
   keys = Object.keys(gameData)
   return (
@@ -53,11 +75,31 @@ function Home() {
         gameData.length <= 0 ? (
           <h3>Loading...</h3>) : 
         (
+
+
+          /*
           <div className="products-box">
             {
               keys.map(key => <Product data={gameData[key]} key={key}/>)
             }
           </div>
+            */
+            <div>
+              <section className='container'>
+                <div className='Slide-holder'>
+                  <div className='slider'>
+                  {slides[slideIndex]}
+                  </div>
+                  <div className='slider-nav'>
+                    <a href="#slide1"></a>
+                    <a href="#slide2"></a>
+                  </div>
+                </div>
+              </section>
+              {keys.map(key => <Product data={gameData[key]} key={key}/>) } 
+            </div>
+
+
         )
       )}
     </div>
